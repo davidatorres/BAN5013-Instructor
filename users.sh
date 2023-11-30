7c1267424a6b4605dd44d16f9836b6caa4d11339
David Torres <me@davidtorres.net>
2023-11-30 13:57:34 -0700

    update user script

---
+++ users.sh
@@ -0,0 +1,25 @@
+# Variables
+csv_file="users.csv"  # Path to the CSV file
+tenant_id="296ca264-a31d-4ced-a5bb-cf30157d52bd"  # Azure AD tenant ID
+group_name="Students"  # Name of the Azure AD group to which the users will be added
+
+# Read the CSV file and create users
+while IFS=',' read -r first_name last_name user_name password; do
+    # Create the users
+    display_name="$first_name $last_name"
+    user_principal_name="$user_name@dat.ms"
+    user_id=$(az ad user create \
+        --display-name "$display_name" \
+        --user-principal-name $user_principal_name \
+        --password $password \
+        --force-change-password-next-sign-in false \
+        --query "id" \
+        --output tsv)
+
+    # Add the user to the group
+    az ad group member add \
+        --group $group_name \
+        --member-id $user_id
+
+    echo "User $user_id $user_principal_name, $display_name, $password, $tenant_id, $group_name created successfully."
+done < "$csv_file"
